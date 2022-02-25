7a8f2696f8202286cd21f95c2db498dd172c05b2
Personal <Personal@THEGOMEZ1113>
2022-02-25 18:51:45 -0500

    Add project files.

---
+++ FirstActivity_POO/ComposedProduct.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstActivity_POO
+{
+    internal class ComposedProduct : Product
+    {
+        public float Discount { get; set; }
+
+        public string Products { get; set; }
+
+        public override decimal ValueToPay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FirstActivity_POO/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using FirstActivity_POO;
+Console.WriteLine("    ");
+Console.WriteLine("  PRODUCTS  ");
+Console.WriteLine("-------------------------------------------------");
+
+Product product1 = new FixedPriceProduct()
+{
+    Description = "Vino Gato Negro",
+    Id = 1010,
+    Price = 46000M,
+    Tax = 0.19F
+};
+Product product2 = new FixedPriceProduct()
+{
+    Description = "Pan Bimbo Artesanal",
+    Id = 2020,
+    Price = 1560M,
+    Tax = 0.19F
+};
+Product product3 = new VariablePriceProduct()
+{
+    Description = "Queso Holandes",
+    Id = 3030,
+    Measurement = "Kilo",
+    Price = 32000M,
+    Quantity = 0.536F,
+    Tax = 0.19F
+};
+Product product4 = new VariablePriceProduct()
+{
+    Description = "Cabano",
+    Id = 4040,
+    Measurement = "Kilo",
+    Price = 18000M,
+    Quantity = 0.389F,
+    Tax = 0.19F
+};
+
+
+
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+Console.WriteLine(product4);
+
